d1cc52e74206a07c15f55437603e72cab68daba8
centos <centos@linux.com>
2023-12-31 06:14:13 -0800

    create practise directory

---
+++ practise/test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <list>
+#include <stack>
+#include <memory>
+#include <algorithm>
+#include <functional>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
+int main()
+{
+    cout << __cplusplus << endl;
+    return 0;
+}
